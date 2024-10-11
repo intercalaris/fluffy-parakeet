2dd262b370ef919ff22c3bf6204b65325a6a92ab
Yonatan Mateo Aviv <122502051+intercalaris@users.noreply.github.com>
2024-10-10 20:04:25 -0400

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -1,8 +1,8 @@
-# Sentence Smash
-# Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
 
-# Example
-# ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 function smash (words) {
    return words.join(' ')
 };
