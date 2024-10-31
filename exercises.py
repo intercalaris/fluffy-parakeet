4172d9b3f95118492bc66c2b3cef86b5e80c00f0
Yonatan Mateo Aviv <122502051+intercalaris@users.noreply.github.com>
2024-10-30 22:26:17 -0400

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -200,10 +200,10 @@ function greet(name){
   return `Hello, ${name} how are you doing today?`
 }
 
-
 function past(h, m, s){
   return (h*60*60*1000 + m*60*1000 + s*1000)
 }
+
 
 function findNeedle(haystack) {
   return "found the needle at position " + haystack.indexOf("needle");
